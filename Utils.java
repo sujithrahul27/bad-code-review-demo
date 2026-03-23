366fe7c1f9b5cca785d0d7064367fbe849e50707
SUJIT RAHUL K <76150646+sujithrahul27@users.noreply.github.com>
2026-03-23 12:18:02 +0530

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,18 @@
+public class Utils {
+
+    public static int calculate(int a, int b) {
+        // unclear logic
+        int result = 0;
+
+        for (int i = 0; i < b; i++) {
+            result += a;
+        }
+
+        return result;
+    }
+
+    public static void printSomething() {
+        // meaningless method
+        System.out.println("Hello");
+    }
+}
